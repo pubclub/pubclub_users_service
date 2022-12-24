7ec570d36ef24aa1734ff4179c28f0a80cf9ced4
Ed Morrell <morrell53@Eds-MacBook-Pro.local>
2022-12-24 19:01:21 +0000

    Commit of module for deploying lambdas with access to dynamodb

--- terraform/lambda_function/variables.tf
+++ terraform/lambda_function/variables.tf
@@ -1,14 +1,14 @@
 variable "function_filename" {
-  type = string
+  type        = string
   description = "Path to zip file containing lambda function"
 }
 
 variable "function_name" {
-  type = string
+  type        = string
   description = "Name of lambda function"
 }
 
 variable "environment_variables" {
-  type = map(string)  
+  type        = map(string)
   description = "Key-values for any environment variables to use in function"
 }
